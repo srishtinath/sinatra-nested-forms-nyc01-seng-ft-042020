246ec70182db5781a2475bbc7ed9975dc6deccc6
Srishti Nath <srishti.nath@gmail.com>
2020-05-13 18:15:32 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+    @@all = []
+
+    def initialize(hash)
+        @name = hash[:name]
+        @weight = hash[:weight]
+        @height = hash[:height]
+        Pirate.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        Pirate.all.clear
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(hash)
+        @name = hash[:name]
+        @type = hash[:type]
+        @booty = hash[:booty]
+        Ship.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        Ship.all.clear
+    end
+
 end
